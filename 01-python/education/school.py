36b2248a4891658943ee50bebb93647191e78b20
liudarren3 <51172901+liudarren3@users.noreply.github.com>
2021-02-02 19:08:55 -0500

    Added school types, added chem majors

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,4 +1,11 @@
+import random
+
 def draw_school():
-    print("This is a school")
+
+    school_type = ['elementary','middle','high']
+
+    print("This is a " + random.choice(school_type) + 'school')
     print("look at its students")
+
+    print('Wow a chem major ')
     return
